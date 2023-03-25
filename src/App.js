dc8d0e90a64fcaf4340539ac66e5204e16ff5f72
Hyojina <hjkim3009@gmail.com>
2023-03-25 23:11:11 +0900

    study: Themes
    1. ThemeProvider
    - index.js파일에서 ThemeProvider로 App 컴포넌트를 감싸준다.
    - ThemeProvider 컴포넌트에 theme 속성을 주고, css 값이 있는 객체를 속성값으로 준다.
    - 중요한건, 다크모드와 라이트모드를 만들어야하므로 property 이름은 똑같아야 한다.
    - 이렇게 하면 ThemeProvider안에 있는 모든 컴포넌트에서 객체에 접근할 수 있게 된다.
    
    2. 컴포넌트에서 접근하기
    - App 컴포넌트에서 styled component로 만든 컴포넌트를 props로 접근할 수 있다.

--- src/App.js
+++ src/App.js
@@ -4,49 +4,19 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
-// 애니메이션을 keyframs로 만들고 그대로 사용할 수 있다.
-const rotationAnimation = keyframes`
-  0%{
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50%{
-    transform: rotate(360deg);
-    border-radius: 100px;
-  }
-  100%{
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: green;
-  animation: ${rotationAnimation} 1s linear infinite;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  // 컴포넌트 직접 타겟팅
-  ${Emoji}:hover {
-    font-size: 100px;
-  }
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>😊</Emoji>
-      </Box>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
