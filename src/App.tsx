7e4992d4d051c5e350fc4d92c58c3a43cb798528
Hyojina <hjkim3009@gmail.com>
2023-03-27 21:49:34 +0900

    study: Typing the Props
    1. Interface
    - object의 shape를 설명해주는 역할
    - Interface로 타입 주기
      - 컴포넌트의 props에 type주기: `({ bgColor }: CircleProps)` 혹은  `(props: CircleProps)`
      - styled component의 props에 type주기: `const Container = styled.div<ContainerProps>```
    
    2. Interface vs Prop Types
    - Interface는 TypeScript와 코드가 실행되기 전에 확인해줌
    - Prop Types는 실행 후 에러로 알려줌

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from "react";
-import styled from "styled-components";
-
-const Wrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100vw;
-  height: 100vh;
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
-
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
+import Circle from "./Circle";
 
 const App = () => {
   return (
-    <Wrapper>
-      <Title>Hello</Title>
-    </Wrapper>
+    <div>
+      <Circle bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </div>
   );
 };
 
