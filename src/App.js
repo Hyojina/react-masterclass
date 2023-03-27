7693952bbc39fb5f315aae29b785745b60311c0a
Hyojina <hjkim3009@gmail.com>
2023-03-27 15:05:59 +0900

    fix: 머지 충돌 해결

--- src/App.js
+++ src/App.js
@@ -6,39 +6,8 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-// 애니메이션을 keyframs로 만들고 그대로 사용할 수 있다.
-const rotationAnimation = keyframes`
-  0%{
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50%{
-    transform: rotate(360deg);
-    border-radius: 100px;
-  }
-  100%{
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: green;
-  animation: ${rotationAnimation} 1s linear infinite;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  // 컴포넌트 직접 타겟팅
-  ${Emoji}:hover {
-    font-size: 100px;
-  }
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 
 function App() {
