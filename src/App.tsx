0b24cc0d88d1469708bb941fe5989fc02ea3e4f9
Hyojina <hjkim3009@gmail.com>
2023-03-28 17:45:46 +0900

    study: Forms
    1. event에 타입 추가하는 법
    - 사용할 때, ReactJS 안에 뭐가 있는지 알 수 없기에 구글링을 해야 한다.
    - ex) const onChange = (event: React.FormEvent<HTMLInputElement>) => { }
      - React.FormEvent는 어떤 종류의 element가 이벤트를 발생시킬지 특정할 수 있다.
      - 바로 뒤에 element의 타입을 작성해준다.
    
    2. currentTarget vs target
    - 타입스크립트를 사용하는 사람들은 currentTarget을 사용함

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import React from "react";
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 const App = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setInputValue(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log("hello", inputValue);
+  };
+
   return (
     <div>
-      <Circle bgColor="teal" borderColor="gold" />
-      <Circle bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={inputValue}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Login</button>
+      </form>
     </div>
   );
 };
